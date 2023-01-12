50ae94c0841d948200f6e9f5aea81f01b7f37bd5
Saravana Ezhil N <webdsnr_sarav@hotmail.com>
2023-01-13 00:34:27 +0530

    Add files via upload

--- oldToNew.java
+++ oldToNew.java
@@ -3,23 +3,31 @@ import java.util.*;
 public class oldToNew {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println();
+        System.out.println("Enter array length: ");
         int S = sc.nextInt();
+        System.out.println("Enter array elements: ");
         int arr[] = new int[S];
-        int b = sc.nextInt();
-
+        
         for (int i=0; i<S; i++) {
             arr[i] = sc.nextInt();
         }
-        output(arr,b);
+        System.out.println();
+        System.out.println("Enter any number to add: ");
+        int b = sc.nextInt();
+        System.out.println();
+        System.out.println("New array elements are: ");
+        output(arr, b);
+        System.out.println();
         sc.close();
     }
-
+    
     public static void output(int[] arr, int b) {
-		int arr2[] = new int[arr.length];
+        int arr2[] = new int[arr.length];
         for (int i=0; i<arr.length; i++) {
-			arr2[i] = arr[i]+b;
+            arr2[i] = arr[i]+b;
             System.out.print(arr2[i]+" ");
         }
+        System.out.println();
     }
 }
